d9235716611b120e7ece0ccbde1001434466e172
Om Dev Singh <devsingh640@gmail.com>
2017-11-28 01:38:43 +0530

    initial commit

---
+++ Data_Types/Data_Types.ino
@@ -0,0 +1,12 @@
+boolean ; // bullen is a data type that will contain a true(1) or false(0).
+char ;  // char is a data type that can hold a character. 
+String ;  // carries a chain of characters.
+int ; short ; // signed value between -32768 and 32767.
+long ;  // signed value between -2147483648 and +2147483647.
+float ; double ;  // floating point between -3.4028235e+38 and +3.4028235e+38, double is same as float on arsuino.
+unsigned char ; unsigned byte ; uint8_t ; // ASCII character or unsigned value between 0 and 255.
+unsigned int ; unsigned word ; uint16_t ; // unsigned value betweem 0 and 65535.
+unsigned long ; unsigned uint32_t;  // unsigned value between 0 and 4294967295.
+
+
+
